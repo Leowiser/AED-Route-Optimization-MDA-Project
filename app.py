d135e83422c2dc115d12b1405faf0f5ebe790752
varkolya <adam.varkoly98@gmail.com>
2024-05-13 18:41:51 +0200

    Modify app to show AEDs by default and only move the patient coordinates/location by the inputs

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import dash
-from dash import html
-from dash import dcc
+from dash import html, dcc
 import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
 import numpy as np
@@ -9,28 +8,32 @@ from dash.dependencies import Input, Output
 from Routing_Class import route
 import plotly.express as px
 
-app = dash.Dash(__name__,title='Zambia MDA Project', external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, title='Zambia MDA Project', external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-# Generate 40 random lat and long
-N=40
-lon_resp = np.random.uniform(4.69,4.71,N)
-lat_resp = np.random.uniform(50.85,50.88,N)
+# Generate 40 random lat and long for responders
+N = 40
+lon_resp = np.random.uniform(4.69, 4.71, N)
+lat_resp = np.random.uniform(50.85, 50.88, N)
 
-# Create dataframe from the numpy arrays
-responder = pd.DataFrame({'longitude':lon_resp, 'latitude':lat_resp})
+# Create dataframe for responders
+responder = pd.DataFrame({'longitude': lon_resp, 'latitude': lat_resp})
 
-N=10
-lon_aed = np.random.uniform(4.69,4.71,N)
-lat_aed = np.random.uniform(50.85,50.88,N)
+# Generate 10 random lat and long for AEDs
+N = 10
+lon_aed = np.random.uniform(4.69, 4.71, N)
+lat_aed = np.random.uniform(50.85, 50.88, N)
 
-# Create geodataframe from numpy arrays
-aed = pd.DataFrame({'longitude':lon_aed, 'latitude':lat_aed})
+# Create dataframe for AEDs
+aed = pd.DataFrame({'longitude': lon_aed, 'latitude': lat_aed})
 
 # Coordinates of Leuven, Belgium
 lat = 50.8798
 lon = 4.7005
 
-
+# Create a map figure
+fig = px.scatter_mapbox(aed, lat="latitude", lon="longitude", color_discrete_sequence=["green"])
+fig.update_traces(marker=dict(size=7))
+'''
 # Create a map figure
 fig = go.Figure(go.Scattermapbox(
     lat=[lat],
@@ -40,24 +43,40 @@ fig = go.Figure(go.Scattermapbox(
         size=14
     ),
 ))
-# fig = px.line_mapbox(lat=[lat], lon=[lon])
+'''
+
+fig.add_trace(go.Scattermapbox(
+    lat=[lat],
+    lon=[lon],
+    mode='markers',
+    marker=go.scattermapbox.Marker(
+        size=10,
+        color='red',  # Color of the new point
+        symbol='circle',  # Symbol of the new point
+        opacity=0.8,
+    ),
+    name='Patient'  # Name of the new point for legend
+))
+
+# fig = px.scatter_mapbox(aed, lat="latitude", lon="longitude", zoom=3, height=300, color_discrete_sequence=["green"])
+# fig.update_traces(marker=dict(size=7))
 
 # Update layout to use mapbox style and set initial zoom level
 fig.update_layout(
     mapbox_style="carto-positron",
     mapbox_center={"lat": lat, "lon": lon},
-    mapbox_zoom=14,
+    mapbox_zoom=13,
+    height=500,
     margin={"r": 0, "t": 0, "l": 0, "b": 0}
 )
-
 
 # lat-long user inputs
 latitude_input = dcc.Input(
     id='latitude_input',
     type='number',
     value=lat,
-    step = 0.0001,
-    debounce = True,
+    step=0.0001,
+    debounce=True,
     style={'width': '100%'}
 )
 
@@ -65,8 +84,8 @@ longitude_input = dcc.Input(
     id='longitude_input',
     type='number',
     value=lon,
-    step = 0.0001,
-    debounce = True,
+    step=0.0001,
+    debounce=True,
     style={'width': '100%'}
 )
 
@@ -101,15 +120,15 @@ app.layout = dbc.Container(
 )
 
 @app.callback(
-    Output('id_title','children'),
-    Output('id_graph','figure'),
+    Output('id_title', 'children'),
+    Output('id_graph', 'figure'),
     [Input('latitude_input', 'value'),
-     Input('longitude_input','value')
+     Input('longitude_input', 'value')
      ]
 )
-
 def update_chart(latitude_value, longitude_value):
-    updated_fig = fig.update_traces(lat=[latitude_value], lon=[longitude_value])
+    updated_fig = fig.update_traces(lat=[latitude_value], lon=[longitude_value], selector=dict(name='Patient'))
+    updated_fig.update_layout(mapbox_center={"lat": latitude_value, "lon": longitude_value})    # center plot around the new coordinates
     # test = route()
     # updated_fig = fig.update_traces(test.send_responders((longitude_value, latitude_value), responder, aed))
     return 'Coordinates of the patient (latitude, longitude): (' + str(latitude_value) + ',' + str(longitude_value) +')', updated_fig
