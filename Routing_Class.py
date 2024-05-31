ceee6afab9a07094160215bf7e2876058e067f68
Leowiser <leon.weiser14@gmail.com>
2024-05-31 13:31:26 +0200

    Update Routing_Class.py
    Bug fix

--- Routing_Class.py
+++ Routing_Class.py
@@ -171,18 +171,16 @@ class route:
                         on='key').drop('key', axis=1)
         # Similar as before calculate the distance between AED and the responders.
         df_merged['dist_responder'] = df_merged.apply(lambda row: geopy.distance.distance(row['Responder_loc'], row['AED_coordinates']).meters, axis=1)
+        # And the distance between the Pateient and the AED
         df_merged['dist_AED'] = df_merged.apply(lambda row: geopy.distance.distance(row['AED_coordinates'], row['Patient_loc']).meters, axis=1)
+        # Now also the distance between AED and Patient is taken
         df_merged['dist_throughAED'] = df_merged['dist_responder'] + df_merged['dist_AED']
 
         if len(df_merged) < 40:
             df_merged['duration_through_AED']=[self.directions([df_merged['Responder_loc'][i], df_merged['AED_coordinates'][i],df_merged['Patient_loc'][i]])['duration'] for i in range(len(df_merged['dist_AED']))]
         else:
-            df_merged_else = pd.DataFrame()
-            for i in pd.unique(df_merged['Responder_loc']):
-                subset = df_merged[df_merged['Responder_loc']==i].nsmallest(7,'dist_throughAED').reset_index(drop = True)
-                df_merged_else = pd.concat([df_merged_else, subset])
-        df_merged = df_merged_else
-        df_merged = df_merged.reset_index(drop = True)
+            subset = [df_merged[df_merged['Responder_loc']==i].nsmallest(7,'dist_throughAED').reset_index(drop = True) for i in pd.unique(df_merged['Responder_loc'])]
+            df_merged = pd.concat(subset).reset_index(drop=True)
         # If the Responders are closer to the AED than the threshold (by default 700 meters as the bird flies, as this takes around 10 minutes to walk),
         # the duration by foot form the responder through the AED to the patient is calculated and stored in the Data Frame.
         df_merged['duration_through_AED']=[self.directions([df_merged['Responder_loc'][i], df_merged['AED_coordinates'][i],df_merged['Patient_loc'][i]])['duration'] for i in range(len(df_merged['dist_AED']))]
